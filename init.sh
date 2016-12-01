a7c3ac0c5701b12a9a3c6c3e949d7b2c61d8a375
RealLukeMartin <lukemartinwebdev@gmail.com>
2016-11-30 20:37:43 -0600

    add dinghy support

--- init.sh
+++ init.sh
@@ -11,9 +11,21 @@ if ! command_exists docker; then
   brew cask install docker-toolbox
 fi
 
+if ! command_exists dinghy; then
+  echo 'installing dinghy...'
+  brew tap codekitchen/dinghy
+  brew install dinghy
+fi
+
+dinghy create --provider virtualbox
+eval $(dinghy env)
+
 git clone git@github.com:poetic/drupal-project.git drupal
 cd drupal
+composer run-script pre-install-cmd
 composer install
+composer run-script drupal-scaffold
+composer run-script post-install-cmd
 cd ..
 
 docker-compose up -d
